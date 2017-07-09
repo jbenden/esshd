19cc1da79dacf67de54c23e4c0d0a566fb639486
Joseph Benden <joe@benden.us>
2017-07-09 12:23:06 -0700

    Added start/0 and stop/0 functions

--- lib/esshd.ex
+++ lib/esshd.ex
@@ -15,4 +15,20 @@ defmodule Sshd do
   def hello do
     :world
   end
+
+  @doc """
+  Starts the `:esshd` Application.
+  """
+  @spec start() :: :ok | {:error, any}
+  def start do
+    Application.start(:esshd)
+  end
+
+  @doc """
+  Stops the running `:esshd` Application.
+  """
+  @spec stop() :: :ok | {:error, any}
+  def stop do
+    Application.stop(:esshd)
+  end
 end
